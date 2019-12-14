51414ccbcc23a1d914d9b0b8be702ef9930cabd1
Nelson Roque <nelsonroquejr@gmail.com>
2019-12-14 15:43:04 -0500

    up

--- R/score_symbol_search.R
+++ R/score_symbol_search.R
@@ -3,16 +3,12 @@
 #' @export
 #' @import tidyverse
 score_symbol_search <- function(data) {
-  #' SCRIPT OVERVIEW
-  #' Symbol Search is mostly scored as it comes from the application.
-  #' This script simply calculates accuracy
-  #' type convert response_time column if not numeric
-  #' parameter validation: verify data passed in is data.frame or tibble
+  # check if data.frame or tibble
   if(is_data_frame_tibble(data)) {
     scored <- data %>% 
       mutate(accuracy = ifelse(user_response == correct_response, 1, 0))
   } else {
-    #' if data is valid, stop code execution and present an error message
+    # raise error if not a data.frame or tibble
     stop("`data` is not a data.frame or tibble. Please try again.")
   }
   return(scored)
--- R/summary_symbol_search.R
+++ R/summary_symbol_search.R
@@ -3,17 +3,29 @@
 #' @export
 #' @import tidyverse
 summary_symbol_search <- function(data, group_var, var_prefix = "symbol_search", basic_outcomes = T, lure_label = "LURE", normal_label = "NORMAL") {
-  if(basic_outcomes){
-    summary_data <- data %>%
-      group_by_(.dots = group_var) %>%
-      mutate(accuracy = ifelse(user_response == correct_response,1,0)) %>%
-      summarise(median_RT_correct_trials = median(response_time[accuracy == 1], na.rm=T),
-                median_RT_error_trials = median(response_time[accuracy == 0], na.rm=T),
-                n_accurate.trials = sum(accuracy),
-                n_error.trials = n() - sum(accuracy),
-                n_trials = n())
+  # check if data.frame or tibble
+  if(is_data_frame_tibble(data)) {
+    # check if accuracy column exists
+    if(is_col_present(data=data,colname = "accuracy")) {
+      if(basic_outcomes){
+        summary_data <- data %>%
+          group_by_(.dots = group_var) %>%
+          summarise(median_RT_correct_trials = median(response_time[accuracy == 1], na.rm=T),
+                    median_RT_error_trials = median(response_time[accuracy == 0], na.rm=T),
+                    n_accurate.trials = sum(accuracy),
+                    n_error.trials = n() - sum(accuracy),
+                    n_trials = n())
+      }
+    } else {
+      # raise error if `data` not scored
+      stop("Please score `data` first. Please try again.")
+    }
+
+  } else {
+    # raise error if not a data.frame or tibble
+    stop("`data` is not a data.frame or tibble. Please try again.")
   }
-  
+
   # make sure that all non-ID columns have a prefix that is unique to the task
   summary_data <- append_colname_prefix(summary_data, group_var, var_prefix)
   return(summary_data)
