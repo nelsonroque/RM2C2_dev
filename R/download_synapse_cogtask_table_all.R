ea51356f2c77168f98ec05524d4c86b87e47aa50
Nelson Roque <nelsonroquejr@gmail.com>
2020-02-19 11:47:29 -0500

    up

--- R/download_synapse_cogtask_table_all.R
+++ R/download_synapse_cogtask_table_all.R
@@ -1,12 +1,13 @@
 #' RM2C2dev
-#' @name download_synapse_table_all
+#' @name download_synapse_cogtask_table_all
 #' @export
 #' @param synapse_email class: string
 #' @param synapse_pw class: string
 #' @param synapse_id class: string
+#' @param uuids class: vector
 #' @import tidyverse
 #' @examples
-#' download_synapse_table_all(synapse_email = NA, synapse_pw = NA, synapse_id = NA, record_uuids = NA)
+#' download_synapse_cogtask_table_all(synapse_email = NA, synapse_pw = NA, synapse_id = NA, record_uuids = NA)
 download_synapse_cogtask_table_all <- function(synapse_email = NA, synapse_pw = NA, synapse_id = NA, uuids=NA) {
   if(!require(synapser)) {
     print("ERROR: Missing `synapser` package. Please run: install.packages('synapser', repos=c('https://sage-bionetworks.github.io/ran', 'http://cran.fhcrc.org'))")
@@ -17,13 +18,16 @@ download_synapse_cogtask_table_all <- function(synapse_email = NA, synapse_pw = 
     synLogin(email = synapse_email, password = synapse_pw, rememberMe = T)
     
     #' build the query
-    base_query <- "select * from %s WHERE game_uuid IN (%s)"
-    uuid_query <- paste0(sQuote(uuids), collapse=",")
+    base_query <- "select * from %s WHERE cogtask_run_uuid IN (%s)"
+    clean_uuids <- uuids[!is.na(uuids)]
+    print(clean_uuids)
+    uuid_query <- paste0(sQuote(clean_uuids, "single"), collapse=",")
     query <- sprintf(base_query, synapse_id, uuid_query)
+    print(query)
     results <- synTableQuery(query)
     
     #' get raw, unmanipulated dataframe matching query results
-    table <- as.data.frame(results)
+    table <- results$asDataFrame()
   }
   return(table)
 }
