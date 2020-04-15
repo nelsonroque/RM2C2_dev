0cd53d18e34a8ea87d21a86c4a3644ff3bf4e2ac
Nelson Roque <nelsonroquejr@gmail.com>
2020-04-15 16:43:01 -0400

    up

--- R/install_synapser.R
+++ R/install_synapser.R
@@ -7,7 +7,7 @@ install_synapser <- function() {
   if(!require(synapser)){
     print("Installing `synapser` and dependency `PythonEmbedInR`. This may take a while.")
     install.packages("PythonEmbedInR", repos=c("http://cran.fhcrc.org", "http://ran.synapse.org"))
-    install.packages('synapser', repos=c('https://sage-bionetworks.github.io/ran', 'http://cran.fhcrc.org'))
+    install.packages("synapser", repos=c("http://ran.synapse.org", "http://cran.fhcrc.org"))
   } else {
     stop("`synapser` already installed.")
   }
