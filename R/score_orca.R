a9375e33ecd6b94c46abdd7aaf3dc90300866e45
Nelson Roque <nelsonroquejr@gmail.com>
2020-12-16 12:50:00 -0500

    add orca scoring to package

---
+++ R/score_orca.R
@@ -0,0 +1,32 @@
+#' RM2C2dev
+#' @name score_orca
+#' @export
+#' @import tidyverse
+score_orca <- function(data) {
+  
+  # check if data.frame or tibble
+  if(is_data_frame_tibble(data)) {
+    
+    # score the data
+    scored <- data %>%
+      mutate(response_label = ifelse(response == 0, "no-match", "match"),
+             correct_response_label = ifelse(correct_response == 0, "no-match", "match")) %>%
+      mutate(response_accuracy = ifelse(response_label == correct_response_label, T, F))
+    
+  } else {
+    
+    # raise error if not a data.frame or tibble
+    stop("`data` is not a data.frame or tibble. Please try again.")
+  }
+  
+  # add processing hash and timestamp
+  scored <- scored %>%
+    append_process_cols()
+  
+  # add scored attribute
+  scored <- add_data_tag(scored, tag_name="is_m2c2_scored", tag_value=T)
+  
+  # =======================================
+
+  return(scored)
+}
