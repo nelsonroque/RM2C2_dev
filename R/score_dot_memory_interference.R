c529baaa75857d29343aca06e0bdf98e87f67833
Nelson Roque <nelsonroquejr@gmail.com>
2021-01-28 15:01:34 -0500

    update to include DM scoring

---
+++ R/score_dot_memory_interference.R
@@ -0,0 +1,37 @@
+#' RM2C2dev: Scoring, Summarizing
+
+#' @name score_dot_memory_interference
+#' @export
+score_dot_memory_interference <- function(df, nontap_cols = c(1:33,35:37), tap_col = c(34)) {
+  restructured <- data.frame()
+  for(row in 1:nrow(df)){
+    row.values.nontap <- df[row,nontap_cols]
+    row.values.tap <- df[row,tap_col]
+    
+    raw.vals <- data.frame(il = unlist(strsplit(row.values.tap$interference_locations, " ")))
+    vals.df <- raw.vals %>% separate(il, c("timestamp", "interf_page", "tap_column", "tap_row", "tap_correct", "pixel_x", "pixel_y"))
+    
+    vals.df$tap_number <- seq(1,nrow(vals.df),1)
+    export.row <- cbind(row.values.nontap,vals.df)
+    restructured <- rbind(restructured,export.row)
+  }
+  
+  restructured <- restructured %>%
+    mutate(interf_page = as.numeric(as.character(interf_page)),
+           tap_column = as.numeric(as.character(tap_column)),
+           tap_row = as.numeric(as.character(tap_row)),
+           pixel_x = as.numeric(as.character(pixel_x)),
+           pixel_y = as.numeric(as.character(pixel_y)),
+           tap_correct = as.numeric(as.character(tap_correct))) %>%
+    mutate(is_data_restructured = TRUE)
+  
+  
+  # add processing hash and timestamp
+  scored <- restructured %>%
+    append_process_cols()
+  
+  # add scored attribute
+  scored <- add_data_tag(scored, tag_name="is_m2c2_scored", tag_value=T)
+  
+  return(scored)
+}
---
+++ R/summary_dot_memory_interference.R
@@ -0,0 +1,47 @@
+#' RM2C2dev
+#' @name summary_dot_memory_interference
+#' @export
+#' @import tidyverse
+summary_dot_memory_interference <- function(data, group_var, var_prefix = "dot_memory", experimental = F) {
+  
+  # check if data.frame or tibble
+  if(is_data_frame_tibble(data)) {
+    
+    # check if data is scored
+    if(is_data_tag_valid(data, tag_name = "is_m2c2_scored", tag_value = T)) {
+      
+      # produce primary outcome summary
+      summary_data <- data %>%
+        group_by(.dots = group_var) %>%
+        summarise(n_correct_taps = sum(tap_correct[tap_correct == 1]),
+                  n_total_taps = n()) %>%
+        mutate(n_incorrect_taps = n_total_taps - n_correct_taps)
+      
+      if(experimental) {
+        stop("Sorry, this function does not yet have experimental output.")
+      }
+      
+    } else {
+      
+      # raise error if `data` not scored
+      stop("Please score `data` first. Please try again.")
+    }
+    
+  } else {
+    
+    # raise error if not a data.frame or tibble
+    stop("`data` is not a data.frame or tibble. Please try again.")
+  }
+  
+  # make sure that all non-ID columns have a prefix that is unique to the task
+  summary_data <- append_colname_prefix(summary_data, group_var, var_prefix)
+  
+  # add processing hash and timestamp
+  summary_data <- summary_data %>%
+    append_process_cols()
+  
+  # add summary attribute
+  summary_data <- add_data_tag(summary_data, tag_name="is_m2c2_summary", tag_value=T)
+  
+  return(summary_data)
+}
