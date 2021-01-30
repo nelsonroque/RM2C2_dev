240fabcdc2e558840694a199d1f77e577893f61f
Nelson Roque <nelsonroquejr@gmail.com>
2021-01-29 21:20:41 -0500

    modified for performance
    unnest > for loops!

--- R/score_dot_memory_interference.R
+++ R/score_dot_memory_interference.R
@@ -2,27 +2,18 @@
 
 #' @name score_dot_memory_interference
 #' @export
-score_dot_memory_interference <- function(df, nontap_cols = c(1:33,35:37), tap_col = c(34)) {
-  restructured <- data.frame()
-  for(row in 1:nrow(df)){
-    row.values.nontap <- df[row,nontap_cols]
-    row.values.tap <- df[row,tap_col]
-    
-    raw.vals <- data.frame(il = unlist(strsplit(row.values.tap$interference_locations, " ")))
-    vals.df <- raw.vals %>% separate(il, c("timestamp", "interf_page", "tap_column", "tap_row", "tap_correct", "pixel_x", "pixel_y"))
-    
-    vals.df$tap_number <- seq(1,nrow(vals.df),1)
-    export.row <- cbind(row.values.nontap,vals.df)
-    restructured <- rbind(restructured,export.row)
-  }
+score_dot_memory_interference <- function(df) {
   
-  restructured <- restructured %>%
-    mutate(interf_page = as.numeric(as.character(interf_page)),
-           tap_column = as.numeric(as.character(tap_column)),
-           tap_row = as.numeric(as.character(tap_row)),
-           pixel_x = as.numeric(as.character(pixel_x)),
-           pixel_y = as.numeric(as.character(pixel_y)),
-           tap_correct = as.numeric(as.character(tap_correct))) %>%
+  restructured <- .df %>%
+    mutate(interference_locations = strsplit(as.character(interference_locations), " ")) %>% 
+    unnest(interference_locations) %>%
+    separate(interference_locations, c("timestamp", "interf_page", "tap_column", "tap_row", "tap_correct", "pixel_x", "pixel_y")) %>%
+    mutate(interf_page = as.numeric(interf_page),
+           tap_row = as.numeric(tap_row),
+           tap_column = as.numeric(tap_column),
+           tap_correct = as.numeric(tap_correct),
+           pixel_x = as.numeric(pixel_x),
+           pixel_y = as.numeric(pixel_y)) %>% 
     mutate(is_data_restructured = TRUE)
   
   # add processing hash and timestamp
