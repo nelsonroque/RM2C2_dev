6ba00471d6176f162eda335a8eca393df807fa93
Nelson Roque <nelsonroquejr@gmail.com>
2021-05-09 19:28:43 -0400

    added @export

--- R/create_n_batches.R
+++ R/create_n_batches.R
@@ -1,3 +1,6 @@
+#' RM2C2dev
+#' @name create_n_batches
+#' @export
 create_n_batches <- function(.data, batches = 10) {
   batch_list = split(.data, ceiling(seq_along(.data)/batches))
   return(batch_list)
